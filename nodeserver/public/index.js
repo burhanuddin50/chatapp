beb8356faf6b2f9b6afbbb06efbf53fac9a209ce
Burhanuddin <burhanuddintinwala43@gmail.com>
2022-09-30 16:48:13 +0530

    Responsive and added speech to text feature

--- nodeserver/public/index.js
+++ nodeserver/public/index.js
@@ -2,6 +2,9 @@ const socket=io('http://localhost:8000');
 const form=document.querySelector('.submitbar');
 const messageinp=document.querySelector('.in');
 const messagecont=document.querySelector('.container');
+var speechRecognition=window.webkitSpeechRecognition;
+const recognition =new speechRecognition();
+recognition.continuous=true;
 var audio= new Audio("Notification.mp3");
 socket.emit('new-user-joined',name);
 const append= (message,position,user)=>{
@@ -18,13 +21,15 @@ form.addEventListener('submit',(e)=>{
     socket.emit('send',`${messageinp.value}`);
     messageinp.value='';
     messagecont.scrollTop = messagecont.scrollHeight;
+    recognition.stop();
+    document.querySelector(".end").style.display="none";
 })
   
 socket.on('user-joined',(name,users,photos)=>{
         append("Joined the chat","left",`${name}`);
         document.querySelector(".only-users").innerHTML="";
        Object.keys(photos).forEach(element => {
-      document.querySelector(".only-users").innerHTML+=`<div class="use"><img src="${photos[element]}" alt="" srcset="" id="pht">${users[element]}</div>`;
+      document.querySelector(".only-users").innerHTML+=`<div class="use"><img src="${photos[element]}" alt="" srcset="" id="pht"><span  id="text_u">${users[element]}</span></div>`;
         
     });
 })
@@ -44,7 +49,7 @@ socket.on('user-left', user=>{
 socket.on("user-left-1",(users,photos)=>{
     document.querySelector(".only-users").innerHTML="";
        Object.keys(photos).forEach(element => {
-      document.querySelector(".only-users").innerHTML+=`<div class="use"><img src="${photos[element]}" alt="" srcset="" id="pht" ><span class="txt"> ${users[element]}</span></div>`;
+      document.querySelector(".only-users").innerHTML+=`<div class="use"><img src="${photos[element]}" alt="" srcset="" id="pht"><span  id="text_u">${users[element]}</span></div>`;
 })})
 document.querySelector(".drop").addEventListener("click",()=>{
     if(document.getElementById("menu-drop").style.display==="none"){
@@ -57,4 +62,23 @@ document.querySelector(".drop").addEventListener("click",()=>{
 
 document.getElementById("clr").addEventListener("click",()=>{
     messagecont.innerHTML ="";
+})
+document.querySelector(".cross").addEventListener("click",()=>{
+    document.querySelector(".users").style.display="none";
+    document.querySelector(".dt").style.display="block";
+})
+document.querySelector(".dt").addEventListener("click",()=>{
+    document.querySelector(".users").style.display="block";
+    document.querySelector(".dt").style.display="none";
+})
+document.querySelector(".vc").addEventListener("click",()=>{
+    recognition.start();
+    document.querySelector(".end").style.display="block";
+})
+document.querySelector(".end").addEventListener("click",()=>{
+    recognition.stop();
+    document.querySelector(".end").style.display="none";
+})
+recognition.addEventListener("result",(event)=>{
+    messageinp.value+=`${event.results[event.resultIndex][0].transcript}`;
 })
